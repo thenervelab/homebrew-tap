4b1a7b47d7bdb7d9bcde44d71b7f91860092d8b3
Dubs <julien.du.bois@starkleytech.com>
2025-02-07 00:07:45 +0400

    include arm mac

--- Formula/hipc.rb
+++ Formula/hipc.rb
@@ -4,10 +4,17 @@ class Hipc < Formula
   version "0.1.0"
   license "MIT"
 
-  if OS.mac?
-    url "https://github.com/thenervelab/hippius-cli/releases/download/v#{version}/hipc-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "REPLACE_WITH_ACTUAL_SHA" # This will be replaced with actual SHA after first release
-  elsif OS.linux?
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/thenervelab/hippius-cli/releases/download/v#{version}/hipc-#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "REPLACE_WITH_ACTUAL_SHA" # This will be replaced with actual SHA after first release
+    else
+      url "https://github.com/thenervelab/hippius-cli/releases/download/v#{version}/hipc-#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "REPLACE_WITH_ACTUAL_SHA" # This will be replaced with actual SHA after first release
+    end
+  end
+
+  on_linux do
     url "https://github.com/thenervelab/hippius-cli/releases/download/v#{version}/hipc-#{version}-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "REPLACE_WITH_ACTUAL_SHA" # This will be replaced with actual SHA after first release
   end
