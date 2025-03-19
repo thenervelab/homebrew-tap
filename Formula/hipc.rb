605e95de2c4c5bed93b2a15e91bc0b6a9b8a2e0f
Dubs <julien.du.bois@starkleytech.com>
2025-03-19 12:58:52 +0400

    update the tag

--- Formula/hipc.rb
+++ Formula/hipc.rb
@@ -1,22 +1,22 @@
 class Hipc < Formula
   desc "CLI tool for managing Docker registries and compute resources on Hippius network"
   homepage "https://hippius.com"
-  version "0.0.4"
+  version "0.0.7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/thenervelab/hippius-cli/releases/download/v#{version}/hipc-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "6867f2d5f496bb9686e41136233fa7e33f1743784defd8100e4be8d33abc8d26" # ARM Mac
+      sha256 "ca0ad0aad2b07e05c037fcd503d25f4429455d1f143c2c2988f82c46f75f69f6" # ARM Mac
     else
       url "https://github.com/thenervelab/hippius-cli/releases/download/v#{version}/hipc-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "6786adc2317e38bf0345f99062b559782e9e4fa9a06642a39de13921575c152f" # Intel Mac
+      sha256 "1e360acd9681a77d8911cf4273de062a459eb3f34898934f1b3e59b038cc8ef1" # Intel Mac
     end
   end
 
   on_linux do
     url "https://github.com/thenervelab/hippius-cli/releases/download/v#{version}/hipc-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "71cae55490f1ef4a90fb71638018251a68a6843e157d6b8c107734e10699146d" # Linux
+    sha256 "02628043b55cc610af11ff9d0c59c0449a0013fc3cf9cad75ee661bfeb72ccdd" # Linux
   end
 
   def install
